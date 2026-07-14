51134500f65f6f0e1dc6b38de70132faa4bdc6cf
Bobby Mulligan <robertpmulligan@gmail.com>
2026-07-13 21:03:36 -0400

    Add: longest-consecutive-sequence - submission-1

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-1.py
@@ -0,0 +1,43 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+        
+        
+        unique_numbers = dict()
+        for i, num in enumerate(nums):
+            unique_numbers[num] = i
+
+
+        unions: List[int] = [i for i in range(len(nums))]
+        sizes: List[int] = [1 for i in range(len(nums))]
+
+        def find(x):
+            while x != unions[x]:
+                x = unions[x]
+            return x
+
+        def union(x, y):
+            if find(x) == find(y):
+                return False
+
+            root_x = find(x)
+            root_y = find(y)
+
+            if sizes[root_x] > sizes[root_y]:
+                sizes[root_x] += sizes[root_y]
+                unions[root_y]  = root_x
+            else:
+                sizes[root_y] += sizes[root_x]
+                unions[root_x]  = root_y
+                        
+            return True 
+        
+        for num in nums:
+            if num - 1 in unique_numbers:
+                union(unique_numbers[num], unique_numbers[num-1])
+    
+        return max(sizes)
+
+
+
