c30774caa121209a067b61414b712a39e18421be
Bobby Mulligan <robertpmulligan@gmail.com>
2026-07-14 13:07:01 -0400

    Add: two-integer-sum-ii - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.py
@@ -0,0 +1,27 @@
+class Solution:
+    def _binary_search(self, numbers:List[int], left: int, right: int, target) -> int:
+        while left <= right:
+            mid: int = left + right // 2
+            if numbers[mid] == target:
+                return mid
+            elif numbers[mid] > target:
+                right = mid -1
+            else:
+                left = mid
+
+        return -1
+
+    
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        i: int = 0 
+        while i < len(numbers) - 1:
+            if numbers[i] == numbers[i+1]:
+                i += 1
+                continue
+            target_index: int = self._binary_search(numbers, i+1, len(numbers) - 1, target - numbers[i])
+            if target_index != -1:
+                return [i+1, target_index+1]
+            i += 1
+        
+            
+        return [-1, -1]
