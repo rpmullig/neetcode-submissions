f8b51332bebfae68238473c4ca8f5b660fdf0073
Bobby Mulligan <robertpmulligan@gmail.com>
2026-07-14 12:51:54 -0400

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.py
@@ -0,0 +1,18 @@
+class Solution:
+    
+    def _advance_non_alphanumeric(self, s: str, i: int, increment: int) -> int:
+        while i >= 0 and i < len(s) and not s[i].isalnum():
+            i += increment
+        return i 
+    
+
+    def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
+        l = self._advance_non_alphanumeric(s, l, 1)
+        r = self._advance_non_alphanumeric(s, r, -1)
+    
+        while l < r and s[l].lower() == s[r].lower():
+            l = self._advance_non_alphanumeric(s, l + 1, 1)
+            r = self._advance_non_alphanumeric(s, r - 1, -1)
+        
+        return l >= r
