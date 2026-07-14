996ec588fd4f96684c3f4e0d943dcf55d737fb17
Bobby Mulligan <robertpmulligan@gmail.com>
2026-07-13 20:10:48 -0400

    Add: unionFind - submission-3

---
+++ Data Structures & Algorithms/unionFind/submission-3.py
@@ -0,0 +1,29 @@
+class UnionFind:
+    
+    def __init__(self, n: int):
+        self.data: List[int] = [i for i in range(n)]
+
+    def find(self, x: int) -> int:
+        while x != self.data[x]:
+            x = self.data[x]
+        return x
+
+    def isSameComponent(self, x: int, y: int) -> bool:
+        return self.find(x) == self.find(y)
+
+    def union(self, x: int, y: int) -> bool:
+        root_y = self.find(y)
+        root_x = self.find(x)
+
+        if root_x == root_y:
+            return False
+
+        self.data[x] = root_y
+        return True
+
+    def getNumComponents(self) -> int:
+        output: int = 0 
+        for i in range(len(self.data)):
+            if i == self.data[i]:
+                output += 1
+        return output
