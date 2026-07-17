1df03411e6dfcaac2d856b1e3f9ef30e202dfa91
Bobby Mulligan <robertpmulligan@gmail.com>
2026-07-17 17:14:30 -0400

    Add: find-target-in-rotated-sorted-array - submission-2

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-2.py
@@ -0,0 +1,38 @@
+class Solution:
+
+    def binary_search(self, nums: List[int], target: int, left: int, right: int) -> int:
+        while left < right: 
+            mid = left + (right - left) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        return -1
+
+
+    def search(self, nums: List[int], target: int) -> int:
+        
+        left, right = 0, len(nums) - 1
+
+        while left < right: 
+
+            if nums[left] < nums[right]:
+                return self.binary_search(nums, target, left, right)
+
+            mid = left + (right - left) // 2
+            if nums[mid] == target:
+                return mid
+            elif mid > 0 and nums[mid] > nums[mid -1]:
+                if nums[left] <= target and nums[mid] > target:
+                    return self.binary_search(nums, target, left, mid - 1)
+                else:
+                    return self.binary_search(nums, target, mid + 1, right)
+            elif nums[mid] < nums[right]: 
+                right = mid - 1
+            else:
+                left  = mid + 1
+
+        return -1 
