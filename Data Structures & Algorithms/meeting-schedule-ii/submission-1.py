9b38f573d8289957445de5a7afa1d633d9c0894d
Bobby Mulligan <robertpmulligan@gmail.com>
2026-07-27 18:18:18 -0400

    Add: meeting-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-1.py
@@ -0,0 +1,22 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        sorted(intervals, key=lambda x: (x.start, x.end))
+
+        h = []
+        max_rooms = 0
+        for interval in intervals:
+            while len(h) > 0 and h[0] <= interval.start:
+                heapq.heappop(h)
+
+            heapq.heappush(h, interval.end)
+            max_rooms = max(max_rooms, len(h))
+
+        return max_rooms
