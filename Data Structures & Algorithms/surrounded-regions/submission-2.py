96d428c9da97932d3063b58fd001155fc14baab5
Bobby Mulligan <robertpmulligan@gmail.com>
2026-08-11 17:41:03 -0400

    Add: surrounded-regions - submission-2

---
+++ Data Structures & Algorithms/surrounded-regions/submission-2.py
@@ -0,0 +1,34 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        stack = []
+        overall_visited = set()
+        steps = [[0,1], [1,0], [-1,0], [0,-1]]
+
+        for r in range(len(board)):
+            for c in range(len(board[r])):
+                if board[r][c] == "X" or (r, c) in overall_visited:
+                    continue
+                
+                is_surrounded = True 
+                stack.append((r,c))
+                current_visit = set()
+                while len(stack) > 0:
+                    i, j = stack.pop()
+                    if (i, j) in current_visit:
+                        continue
+                    if board[i][j] == "O":
+                        current_visit.add((i,j))
+                        if i == 0 or i == len(board) - 1 or j == len(board[r]) - 1 or j == 0:
+                            is_surrounded = False
+                        for step in steps:
+                            inc_r, inc_c = step[0], step[1]
+                            new_r, new_c = i + inc_r, j + inc_c
+                            if new_r >= 0 and new_r < len(board) and new_c >= 0 and new_c < len(board[r]) and board[new_r][new_c] == "O" and (new_r, new_c) not in current_visit:
+                                stack.append((new_r, new_c))
+                
+                if is_surrounded:
+                    for coords in current_visit:
+                        i, j = coords
+                        board[i][j] = "X"
+                
+                overall_visited.update(current_visit)
