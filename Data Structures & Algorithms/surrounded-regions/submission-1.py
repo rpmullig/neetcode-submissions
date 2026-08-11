f21f3d538d039627ebb8712f9ebc7ec73af325aa
Bobby Mulligan <robertpmulligan@gmail.com>
2026-08-11 17:36:52 -0400

    Add: surrounded-regions - submission-1

---
+++ Data Structures & Algorithms/surrounded-regions/submission-1.py
@@ -0,0 +1,31 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        stack = []
+        overall_visited = set()
+        steps = [[0,1], [1,0], [-1,0], [0,-1]]
+
+        for r in range(len(board)):
+            for c in range(len(board[r])):
+                if board[r][c] == "X" or (r, c) in overall_visited:
+                    continue
+                
+                stack.append((r,c))
+                current_visit = set()
+                while len(stack) > 0:
+                    i, j = stack.pop()
+                    if (i, j) not in current_visit and board[i][j] == "O":
+                        current_visit.add((i,j))
+                        for step in steps:
+                            inc_r, inc_c = step[0], step[1]
+                            new_r, new_c = i + inc_r, j + inc_c
+                            if new_r >= 0 and new_r < len(board) and new_c >= 0 and new_c < len(board[r]) and (new_r, new_c) not in current_visit:
+                                stack.append((new_r, new_c))
+                
+                if len(current_visit) > 1:
+                    for coords in current_visit:
+                        i, j = coords
+                        board[i][j] = "X"
+                
+                overall_visited = current_visit & overall_visited
+
+        
