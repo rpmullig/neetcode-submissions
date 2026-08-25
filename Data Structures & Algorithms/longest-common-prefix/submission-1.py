d0476d623701020452205893f864cbdb2591f4b3
Bobby Mulligan <robertpmulligan@gmail.com>
2026-08-25 15:49:40 -0400

    Add: longest-common-prefix - submission-1

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-1.py
@@ -0,0 +1,27 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        prefix_index = 0
+        prefix = list()
+        still_common = True
+        while still_common:
+            current_letter = None
+            for word in strs:
+                if current_letter:
+                    if prefix_index >= len(word):
+                        still_common = False
+                        continue
+                    
+                    if word[prefix_index] != current_letter:
+                        still_common = False
+                else:
+                    if prefix_index >= len(word):
+                        still_common = False
+                        break
+                    current_letter = word[prefix_index]
+            
+            prefix_index += 1
+            if still_common:
+                prefix.append(current_letter)
+
+
+        return ''.join(prefix)
